0e4d8f495b27544cfa14203464fcae4dabee3289
A-Marwan <amamou.marwan@gmail.com>
2015-05-01 21:34:34 +0200

    Update startShell.sh

--- fig/startShell.sh
+++ fig/startShell.sh
@@ -1,2 +1,2 @@
 #docker pull mar2max/websrv
-docker run -t -i  -v `pwd`:/vagrant --link PROJECTNAME_rabbitmq_1:rabbitmq --link PROJECTNAME_elasticsearch_1:elasticsearch --link PROJECTNAME_mysql_1:mysql mar2max/websrv /bin/bash
+docker run -t -i  -v `pwd`:/appname --link PROJECTNAME_rabbitmq_1:rabbitmq --link PROJECTNAME_elasticsearch_1:elasticsearch --link PROJECTNAME_mysql_1:mysql mar2max/websrv /bin/bash
