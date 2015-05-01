4691960f087606cbd719c3efac8c7675e6fd8e42
A-Marwan <amamou.marwan@gmail.com>
2015-05-01 18:35:29 +0200

    webserv & fig

---
+++ fig/startShell.sh
@@ -0,0 +1,2 @@
+#docker pull alveos/websrv
+docker run -t -i  -v `pwd`:/vagrant --link PROJECTNAME_rabbitmq_1:rabbitmq --link PROJECTNAME_elasticsearch_1:elasticsearch --link PROJECTNAME_mysql_1:mysql mar2max/websrv /bin/bash
